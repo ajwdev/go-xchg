d2f53c8edc3579e7d2c4592834b9eb8689247eff
Andrew Williams <awilliams@intoxitrack.net>
2016-01-16 11:20:32 -0600

    Add a super basic example
    Add a basic example program that demonostrates how to use the library.
    
    Fixes #2

---
+++ examples/atomic_swap.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"fmt"
+	"github.com/TheDude05/go-xchg"
+	"log"
+	"os"
+	"syscall"
+)
+
+func getInode(path string) (uint64, error) {
+	fi, err := os.Lstat(path)
+	if err != nil {
+		return 0, err
+	}
+
+	return fi.Sys().(*syscall.Stat_t).Ino, nil
+}
+
+func main() {
+	if len(os.Args) < 3 {
+		log.Fatal("must provide source and destination files")
+	}
+
+	// Print out the inode numbers pre exchange
+	inode1, err := getInode(os.Args[1])
+	if err != nil {
+		log.Fatalf("error: %s\n", err)
+	}
+	inode2, err := getInode(os.Args[2])
+	if err != nil {
+		log.Fatalf("error: %s\n", err)
+	}
+	fmt.Printf("%s inode: %d\n", os.Args[1], inode1)
+	fmt.Printf("%s inode: %d\n", os.Args[2], inode2)
+
+	// Do the exchange
+	fmt.Println("exchanging...")
+	err = xchg.Exchange(os.Args[1], os.Args[2])
+	if err != nil {
+		if err == syscall.ENOSYS {
+			log.Fatalf("system does not support 'renameat2' system call. error: %s\n", err)
+		}
+
+		log.Fatalf("error: %s\n", err)
+	}
+
+	// Print out the new inode numbers
+	inode1, err = getInode(os.Args[1])
+	if err != nil {
+		log.Fatalf("error: %s\n", err)
+	}
+	inode2, err = getInode(os.Args[2])
+	if err != nil {
+		log.Fatalf("error: %s\n", err)
+	}
+	fmt.Printf("%s inode: %d\n", os.Args[1], inode1)
+	fmt.Printf("%s inode: %d\n", os.Args[2], inode2)
+}
